1478c3841b9abfeee37c0085ff7af95f01e4c9db
Muzammil Hussain <39485686+MuzammiLFive@users.noreply.github.com>
2020-08-11 23:42:22 +0530

    save-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cat-app';
+  cats = [];
+  URL = "https://api.thecatapi.com/v1/images/search";
+  constructor(private http: HttpClient) {  }
+
+  addCat() {
+    return this.http.get(this.URL, {
+      headers: {["x-api-key"]:"206f0a0f-d8cb-43d0-bc39-159b5f5b2e5a"}
+      // params: {["mime_type"]:"gif", ["limit"]:"1"}
+    })
+      .subscribe(result => {
+        result[0].url;
+        this.cats.push(result[0].url);
+      });
+  }
 }
